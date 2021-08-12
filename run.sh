8d204d788a7a525aaee4f8fdae571bd93bb02d07
Bruno Kemmer <brunokemmer@gmail.com>
2021-08-11 22:21:18 -0300

    sending files to gcp

---
+++ run.sh
@@ -0,0 +1,1 @@
+python main.py --mode train --epochs 100 --input /home/bkemmer/projects/data/CACD2_processed/caae/CACD/train --output ./train_outputs/CACD_6groups-caae_z50_100epochs -z 50 --models-saving tail --batch-size 100
